7e261218c53eb67105dcf28b183d5d8b96f505ef
Vyacheslav Tarasov <v.t.45@mail.ru>
2025-02-26 20:53:49 +0700

    added base FSD architecture

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,17 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from '../shared/assets/images/react.svg'
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
